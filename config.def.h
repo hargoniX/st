cde045baf6f4023f5aa6a628e95b66536e0b74a2
Henrik Böving <hargonix@gmail.com>
2021-05-24 18:10:43 +0200

    gruvbox light soft

--- config.def.h
+++ config.def.h
@@ -95,22 +95,22 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#f2e5bc", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
 	"#cc241d",
 	"#98971a",
 	"#d79921",
 	"#458588",
 	"#b16286",
 	"#689d6a",
-	"#a89984",
+	"#7c6f64",
 	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#9d0006",
+	"#79740e",
+	"#b57614",
+	"#076678",
+	"#8f3f71",
+	"#427b58",
+	"#3c3836",
 };
 
 
@@ -120,7 +120,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 15;
 
 /*
